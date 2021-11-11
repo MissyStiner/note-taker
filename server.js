819704c092e7d7193ab471a871791234896a57b5
Missy Stiner <missystiner@gmail.com>
2021-11-11 15:18:30 -0600

    route files push from root

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const apiRoutes = require('./routes/apiRoutes');
-//const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Initialize the app and create a port
 const app = express();
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-//app.use('/api', apiRoutes);
-//app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
